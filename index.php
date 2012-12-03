28eec0b0efbc861c8d130074abd39c0eefca3e81
nicolabricot <github@nicolabricot.com>
2012-12-03 12:35:15 +0100

    Add files first commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="fr-fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Live &bull; Iariss Team</title>
+    
+    <!--
+    
+    $$$$$$\                    $$\                           $$$$$$$$\                                
+    \_$$  _|                   \__|                          \__$$  __|                               
+      $$ |  $$$$$$\   $$$$$$\  $$\  $$$$$$$\  $$$$$$$\          $$ | $$$$$$\   $$$$$$\  $$$$$$\$$$$\  
+      $$ |  \____$$\ $$  __$$\ $$ |$$  _____|$$  _____|         $$ |$$  __$$\  \____$$\ $$  _$$  _$$\ 
+      $$ |  $$$$$$$ |$$ |  \__|$$ |\$$$$$$\  \$$$$$$\           $$ |$$$$$$$$ | $$$$$$$ |$$ / $$ / $$ |
+      $$ | $$  __$$ |$$ |      $$ | \____$$\  \____$$\          $$ |$$   ____|$$  __$$ |$$ | $$ | $$ |
+    $$$$$$\\$$$$$$$ |$$ |      $$ |$$$$$$$  |$$$$$$$  |         $$ |\$$$$$$$\ \$$$$$$$ |$$ | $$ | $$ |
+    \______|\_______|\__|      \__|\_______/ \_______/          \__| \_______| \_______|\__| \__| \__|
+    
+    -->
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" lang="fr" content="IARISS Team, l'équipe IARISS de la nuit de l'info 2012 qui va promouvoir le patrimoine français ! Venez suivre le live de @IarissTeam :)" />
+    <meta name="keywords" content="Live, LiveTweet, Webcam, Video, IARISS, IARISS Team, Nuit de l'info 2012, #nuitinfo, Alsace, Mulhouse, Programmation, Promotion du patrimoine français, patrimoine français"/>
+    <meta name="author" content="IARISS Team" />
+    <meta name="copyright" content="IARISS Team" />
+    <meta name="robots" content="index, follow, archive" />
+    
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+    <link rel="icon" type="image/png" href="/favicon.png" />
+    
+    <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Imprima" type="text/css" />
+<?php /*<link rel="stylesheet" href="css/jquery.countup.css" />*/?>
+    <link rel="stylesheet" href="/css/expectation.css" />
+
+  </head>
+  
+  <body>
+    
+  <div id="container">
+  
+    <h1><a href="http://www.youtube.com/watch?v=EABV7T7NncA">Iariss Team</a></h1>
+  
+  </div><!-- #container -->
+  
+  <!-- Oui, on sait, c'est une machine à gaz que nous chargons pour une fonctionnalité non importante, mais hein on avait envie que ce soit bien centré -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<?php /*<script src="js/jquery.countup.js"></script>
+  <script src="js/scripts.js"></script> */ ?>
+  <script>
+    $(function() {
+    jQuery.fn.center = function () {
+      this.css("position","absolute");
+      this.css("top", Math.max(0, (($(window).height() - this.outerHeight()) / 2) +  $(window).scrollTop()) + "px");
+      this.css("left", Math.max(0, (($(window).width() - this.outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+      return this;
+    }
+    $("#container").css('margin-top', 0).center();
+    });
+  </script>
+  <script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-36456913-3']);
+    _gaq.push(['_trackPageview']);
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  </script>
+  </body>
+</html>
